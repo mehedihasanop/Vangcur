66b73b47fbd4000dbc2b700b776d5663ff5f1b34
mehedihasanop <mh3589605@gmail.com>
2026-05-13 17:38:19 +0600

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -8,7 +8,7 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = "https://gzdujkwjplobonvghyhx.supabase.co/rest/v1/";
 
 // 👇 এখানে আপনার Supabase Public (Publishable) Key পেস্ট করুন
-const SUPABASE_PUBLIC_KEY = "sb_publishable_KF1pFUO6N77m216FxBeyTA_vJQ4Rzok";
+const SUPABASE_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6ZHVqa3dqcGxvYm9udmdoeWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NzQzODUsImV4cCI6MjA5NDE1MDM4NX0.evLb7_gv5vtFrpsyCeX3h6mLxqCuOXQuY-w9ygvDqak";
 
 // ============================================================
 //  Supabase Client — এটি export করা হচ্ছে, যেকোনো ফাইলে import করুন
